5f3e5d41866a1fa351e159d9c90e9d6d89eaa2db
Katelyn Ma <katelynvan152@gmail.com>
2023-03-26 13:02:51 -0700

    updated jest test with mongo

--- server/src/routes/planets/planets.test.js
+++ server/src/routes/planets/planets.test.js
@@ -1,11 +1,22 @@
 const app = require('../../app');
 const request = require("supertest")
+const { mongoConnect, mongoDisconnect } = require('../../services/mongo')
 
-describe('Test for GET /planets', () => {
-    test('It should return a 200 success', async() => {
-        const response = await request(app)
-            .get('/planets')
-            .expect(200)
-            .expect("Content-Type", /json/)
+describe('Planets API', () => {
+    beforeAll(async () => {
+        await mongoConnect();
+    });
+
+    afterAll(async () => {
+        await mongoDisconnect();
+    })
+    
+    describe('Test for GET /planets', () => {
+        test('It should return a 200 success', async() => {
+            const response = await request(app)
+                .get('/planets')
+                .expect(200)
+                .expect("Content-Type", /json/)
+        })
     })
 })
---
+++ server/src/services/mongo.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = 'mongodb+srv://katelynvan152:nwMpAW8m9CwgHeGd@nasacluster.eexxvzn.mongodb.net/?retryWrites=true&w=majority';
+
+//when connection to db success
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready')
+})
+//when connection fails
+mongoose.connection.on('error', err => {
+    console.error(err);
+})
+
+async function mongoConnect() {
+    try {
+        await mongoose.connect(MONGO_URL);
+        console.log('MongoDB connection ready');
+    } catch (err) {
+        console.error('MongoDB connection error', err);
+    }
+}
+async function mongoDisconnect() {
+    await mongoose.disconnect();
+}
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect
+}
